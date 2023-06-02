6040d227872ef05b697347740f6916cac4e8954d
HoooMaC <wawcam123@gmail.com>
2023-06-02 21:11:23 +0700

    add premake

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	systemversion "latest"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("../../bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("../../bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_tables.cpp",
+		"imgui_demo.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_glfw.cpp"
+	}
+
+	includedirs
+	{
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.imgui}"
+	}
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
